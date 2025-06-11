a47678e547d694d0068be2979e597bd3f7d88940
k_takahashi <k_takahashi@LAPTOP-G2AOCJTH>
2025-06-11 15:20:41 +0900

    ログインコード

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -1,5 +1,42 @@
 package dao;
 
-public class TeacherDao extends dao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import bean.Teacher;
+
+/**
+ *
+ * @author k_takahashi
+ *
+ */
+public class TeacherDao extends dao {
+	/**
+	 *
+	 * @param id
+	 * @return
+	 * @throws Exception
+	 */
+	public Teacher get(String id) throws Exception {
+		Teacher Teacher = null;
+
+		try (Connection con = getConnection()) {
+			String sql = "SELECT * FROM TEACHER WHERE NAME = ?";
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, id);
+			ResultSet rs = st.executeQuery();
+
+			if (rs.next()) {
+				Teacher = new Teacher();
+				Teacher.setId(rs.getString("id"));
+				Teacher.setPassword(rs.getString("password"));
+
+	}
+		}
+		return Teacher;
+
+	}
 }
+
+
