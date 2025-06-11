f6e6a52eadc3752ff167eb5dc90b0faffbb303bb
r_katabe <r_katabe@LAPTOP-L6A2OC2U>
2025-06-11 15:08:30 +0900

    e

--- src/dao/SubjectDao.java
+++ src/dao/SubjectDao.java
@@ -1,9 +1,25 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+
+
+
 public class SubjectDao {
 
+	public void delete(int id) throws Exception {
+		try (Connection con = getConnection()) {
+			String sql = "DELETE FROM products WHERE id = ?";
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+			st.executeUpdate();
+		}
+	}
 
-
-
+	private Connection getConnection() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
 
 }
