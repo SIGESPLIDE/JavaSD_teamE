06298339af46e2838391f0b40efd914c9f55e928
k_takahashi <k_takahashi@LAPTOP-G2AOCJTH>
2025-06-12 12:36:59 +0900

    edit  TeacherDao

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -37,6 +37,51 @@ public class TeacherDao extends dao {
 		return Teacher;
 
 	}
+	/**
+	 *
+	 * @param id
+	 * @return
+	 * @throws Exception
+	 */
+	public Teacher findById(String id) throws Exception{
+		Teacher Teacher = null;
+
+		try (Connection con = getConnection()) {
+			String sql = "SELECT * FROM TEACHER WHERE NAME = ?";
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, id);
+			ResultSet rs = st.executeQuery();
+
+			if (rs.next()) {
+				Teacher = new Teacher();
+				Teacher.setId(rs.getString("id"));
+				Teacher.setPassword(rs.getString("password"));
+
+			}
+		}
+
+		return Teacher;
+	}
+	/**
+	 *
+	 * @param id
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean existsid(String id) throws Exception {
+		boolean exists = false;
+
+		try (Connection con = getConnection()) {
+			String sql = "SELECT COUNT(*) FROM TEACHER WHERE NAME = ?";
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, id);
+			ResultSet rs = st.executeQuery();
+
+			if (rs.next() && rs.getInt(1) > 0) {
+				exists = true;
+			}
+		}
+
+		return exists;
+	}
 }
-
-
