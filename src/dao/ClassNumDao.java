e5728cb8749f493f9066034f4c1d0fcac677e56a
SIGES <sigesplide@gmail.com>
2025-06-13 09:37:43 +0900

    editing ClassNumDao

--- src/dao/ClassNumDao.java
+++ src/dao/ClassNumDao.java
@@ -1,5 +1,16 @@
 package dao;
 
+import java.util.List;
+
+/**
+ * 
+ * @author s_saito
+ *
+ */
 public class ClassNumDao extends dao {
 
+	public List<String> filter() throws Exception {
+		return null;
+		
+	}
 }
