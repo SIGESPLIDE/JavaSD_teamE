5f358420f0fc57102cd9de8c43780de27444c346
a_suzuki <a_suzuki@LAPTOP-TTM225D1>
2025-06-19 12:26:20 +0900

    rewrite SubjectCreateController.jaca

--- src/SubjectMain/SubjectCreateController.java
+++ src/SubjectMain/SubjectCreateController.java
@@ -1,22 +1,52 @@
 package SubjectMain;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Teacher;
 import tool.CommonServlet;
 
+/**
+ *
+ * @author a_suzuki
+ *
+ */
+
+@WebServlet("/subject/create/controller")
 public class SubjectCreateController extends CommonServlet {
 
-	@Override
-	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
+    /**
+     * GETリクエストを処理し、科目登録画面にフォワードする
+     */
+    @Override
+    public void get(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // セッションからログインユーザー情報を取得
+        HttpSession session = req.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("user");
 
-	}
+        // ログインしていない場合はログインページにリダイレクト
+        if (teacher == null) {
+            resp.sendRedirect("/score_manage/login"); // ログインページのURLを仮定
+            return;
+        }
 
-	@Override
-	protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
+        // 科目登録フォームのJSPにフォワード
+        // 画面表示に必要なデータがあればここでDAOから取得してリクエストにセットするが、
+        // 今回は単純に画面を表示するだけと想定
+        req.getRequestDispatcher("/subject/subject_create.jsp").forward(req, resp);
+    }
 
-	}
-
+    /**
+     * POSTリクエストはGETリクエストと同じ処理に流す
+     * (フォームの表示のみを行うため)
+     */
+    @Override
+    public void post(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        get(req, resp);
+    }
 }
