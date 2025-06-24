7e75de469bf82282545f87b1c4f0d1f98314868c
SIGES <sigesplide@gmail.com>
2025-06-24 14:04:06 +0900

    fix conflict

--- src/bean/ClassNum.java
+++ src/bean/ClassNum.java
@@ -3,27 +3,6 @@ package bean;
 import java.io.Serializable;
  
 public class ClassNum implements Serializable {
-<<<<<<< Upstream, based on origin/master
-
-	private String class_num;
-	private School school;
-
-	public String getClass_num() {
-		return class_num;
-	}
-	public void setClass_num(String class_num) {
-		this.class_num = class_num;
-	}
-	public School getSchool() {
-		return school;
-	}
-	public void setSchool(School school) {
-		this.school = school;
-	}
-
-}
-
-=======
  
 	private String class_num;
 	private School school;
@@ -42,4 +21,3 @@ public class ClassNum implements Serializable {
 	}
  
 }
->>>>>>> bbbc7b8 complete ClassNum.java
