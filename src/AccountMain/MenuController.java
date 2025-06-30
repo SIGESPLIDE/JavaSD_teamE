9ae92bed06fafeea97e82078307dd3438b66d01d
SIGES <sigesplide@gmail.com>
2025-06-30 15:15:07 +0900

    fix undo

--- src/AccountMain/MenuController.java
+++ src/AccountMain/MenuController.java
@@ -1,15 +1,18 @@
 package AccountMain;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import tool.CommonServlet;
 
+@WebServlet("/menu.action")
 public class MenuController extends CommonServlet {
 
 	@Override
 	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
+        resp.sendRedirect("/JavaSD/main/MMNU001.jsp");
 
 	}
 
