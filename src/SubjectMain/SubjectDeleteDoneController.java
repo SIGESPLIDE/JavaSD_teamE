425c9e23a38715398552e5422a851019de19ada2
a_suzuki <a_suzuki@LAPTOP-TTM225D1>
2025-07-03 12:20:24 +0900

    error correction

---
+++ src/SubjectMain/SubjectDeleteDoneController.java
@@ -0,0 +1,32 @@
+package SubjectMain;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.SubjectDao;
+import tool.CommonServlet;
+
+@WebServlet(urlPatterns = {"/main/SubjectDeleteDone"})
+
+public class SubjectDeleteDoneController extends CommonServlet {
+
+    @Override
+    protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        // 削除完了画面を表示
+        req.getRequestDispatcher("/main/SBJM007").forward(req, resp);
+    }
+
+    @Override
+    protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        // リクエストから科目IDを取得
+        int subjectId = Integer.parseInt(req.getParameter("id"));
+
+        // DAOを使って削除処理を実行
+        SubjectDao dao = new SubjectDao();
+        dao.delete(subjectId);
+
+        // 削除完了画面へリダイレクト
+        resp.sendRedirect("SBJM007");
+    }
+}
